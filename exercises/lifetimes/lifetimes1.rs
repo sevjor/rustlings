e965363dda3a6c53c6b7e25e92c4141189ed7b34
joarse <joarse@kth.se>
2023-11-04 06:54:47 +0100

    Add lifetimes solutions

--- exercises/lifetimes/lifetimes1.rs
+++ exercises/lifetimes/lifetimes1.rs
@@ -8,9 +8,7 @@
 // Execute `rustlings hint lifetimes1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
-fn longest(x: &str, y: &str) -> &str {
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
         x
     } else {
@@ -22,6 +20,6 @@ fn main() {
     let string1 = String::from("abcd");
     let string2 = "xyz";
 
-    let result = longest(string1.as_str(), string2);
+    let result : &str = longest(string1.as_str(), string2);
     println!("The longest string is '{}'", result);
 }
